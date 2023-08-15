06a5511fa723836c36fce3c9032800c276f5528d
kato0116 <j12shun@gmail.com>
2023-08-15 23:51:22 +0900

    [add]functionsに勾配の計算を追加

--- chapter04/common/functions.py
+++ chapter04/common/functions.py
@@ -17,15 +17,47 @@ def get_data(normalize=True,flatten=True,one_hot_label=True,num_class=10):
         
     return (x_train, t_train), (x_test, t_test)
 
+# シグモイド関数
 def sigmoid(x):
     return 1/(1+np.exp(-x))
 
+# softmax関数
 def softmax(x):
     exp_x     = np.exp(x)
     sum_exp_x = np.sum(exp_x)
     y         = exp_x/sum_exp_x
     return y
 
+# 交差エントロピー誤差の計算
 def cross_entropy_error(y,t):
     delta = 1e-7
     return -np.sum(t*np.log(y+delta))
+
+# 勾配の計算
+def numerical_gradient(f, x):
+    h = 1e-4
+    grad = np.zeros_like(x)  # 勾配を0で初期化
+    for i in range(x.size):
+        tmp = x[i]
+        # f(x+h)
+        x[i] = tmp + h
+        fxh1 = f(x)
+        # f(x-h)
+        x[i] = tmp - h
+        fxh2 = f(x)
+
+        # xを元に戻す
+        x[i] = tmp
+        # 勾配
+        grad[i] = (fxh1 - fxh2) / (2 * h)  # 修正: '+' を '-' に変更
+    return grad
+
+# lrは学習率, step_numは試行回数
+def gradient_descent(f, init_x, lr=0.01, step_num=100):
+    x = init_x.copy()
+
+    for i in range(step_num):
+        grad = numerical_gradient(f, x)
+        x = x - lr * grad  # xの更新
+        plt.scatter(x[0],x[1])
+    return x
